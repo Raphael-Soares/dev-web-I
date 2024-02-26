6538b1427a4cbdc5cc9e7e3d817c96d7a4ee5d80
Raphael Soares <soaresphaell@gmail.com>
2024-02-26 13:55:30 -0300

    feat: tabs and forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Progress } from '@/components/ui/progress';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import Date from './components/Date';
+import { Input } from './components/ui/input';
+import { Button } from './components/ui/button';
+import { Plus, Search } from 'lucide-react';
 
 export default function App() {
   return (
@@ -8,8 +14,35 @@ export default function App() {
       <Card className='max-w-6xl w-full'>
         <CardHeader>
           <Date />
+          <Progress value={33} />
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <Tabs defaultValue='done' className=''>
+            <div className='flex justify-between gap-2'>
+              <TabsList className=''>
+                <TabsTrigger value='done'>Done</TabsTrigger>
+                <TabsTrigger value='pending'>Pending</TabsTrigger>
+              </TabsList>
+
+              <div className='flex gap-2'>
+                <Input type='text' placeholder='Search' className='max-w-lg' />
+                <Button variant='outline'>
+                  <Search className='size-4' />
+                </Button>
+              </div>
+            </div>
+
+            <div className='flex  my-4'>
+              <Input type='text' placeholder='Add a new task' className='border-r-0 rounded-tr-none rounded-br-none ' />
+              <Button variant='outline' className='border-l-0 rounded-tl-none rounded-bl-none'>
+                <Plus className='size-5' />
+              </Button>
+            </div>
+
+            <TabsContent value='done'></TabsContent>
+            <TabsContent value='pending'></TabsContent>
+          </Tabs>
+        </CardContent>
         <CardFooter className='flex justify-between'></CardFooter>
       </Card>
     </div>
--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -2,14 +2,18 @@ export default function Header() {
   const date = new Date();
 
   const [day, month, year] = date.toLocaleDateString().split('/');
+
   const [dayName] = date.toLocaleDateString('en-US', { weekday: 'long' }).split(' ');
 
+  const monthName = date.toLocaleDateString('en-US', { month: 'short' });
+
   return (
-    <header className=' flex justify-between'>
-      <div className='flex flex-row'>
+    <header className=' flex justify-between items-center'>
+      <div className='flex flex-row items-center'>
         <span className='text-6xl font-semibold'>{day}</span>
-        <span className='flex flex-col'>
-          <span className='text-xl '>{month}</span>
+
+        <span className='flex flex-col -space-y-2'>
+          <span className='text-2xl font-normal'>{monthName}</span>
           <span className='text-xl font-light '>{year}</span>
         </span>
       </div>
